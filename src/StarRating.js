695fbc232d1e44e9facd0f3c4386ef279dad201a
Noran Sherif <noranosherif958@gmail.com>
2023-10-07 21:43:19 +0300

    add hover property to Star

--- src/StarRating.js
+++ src/StarRating.js
@@ -17,18 +17,21 @@ const textStyle = {
 
 function StarRating({ maxRating = 5 }) {
   const [rating, setRating] = useState(0);
+  const [tempRating, setTempRating] = useState(0);
   return (
     <div style={containerStyle}>
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
             key={i}
-            onClick={() => setRating(i + 1)}
-            full={rating >= i + 1}
+            onRate={() => setRating(i + 1)}
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
           />
         ))}
       </div>
-      <p style={textStyle}>{rating || ""}</p>
+      <p style={textStyle}>{tempRating || ""}</p>
     </div>
   );
 }
@@ -40,9 +43,15 @@ const starStyle = {
   cursor: "pointer",
 };
 
-function Star({ onClick, full }) {
+function Star({ onRate, full, onHoverIn, onHoverOut }) {
   return (
-    <span role="button" style={starStyle} onClick={onClick}>
+    <span
+      role="button"
+      style={starStyle}
+      onClick={onRate}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+    >
       {full ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
