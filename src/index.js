5fc9aecf9605e239146f2b2f8680886ce13de698
Noran Sherif <noranosherif958@gmail.com>
2023-10-07 23:38:51 +0300

    improve component reusability

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 /* import './index.css';
 import App from './App'; */
 import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating maxRating={10} color="blue" onSetRating={setMovieRating} />
+      <p>this movie was Rated {movieRating} Stars</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/*  <App /> */}
 
-    <StarRating maxRating={10} />
-    
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+    <StarRating size="28" color="red" />
+    <Test />
   </React.StrictMode>
 );
